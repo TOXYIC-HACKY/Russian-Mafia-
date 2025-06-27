c67c7f53357003257d0a1759100d4c1a717468c1
Conway-eng <Mornah188@gmail.com>
2025-06-27 19:36:39 +0300

    Update mega.js

--- mega.js
+++ mega.js
@@ -12,8 +12,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'sulakshamadara4@gmail.com', // Replace with your Mega email
-    password: 'Sulakshamd011222@ms', // Replace with your Mega password
+    email: 'nicksonkipruto79@gmail.com', // Replace with your Mega email
+    password: 'Kim2024K', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
